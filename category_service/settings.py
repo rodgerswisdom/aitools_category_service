ded74422feed5ac3ba8179c3517149fda198c9f4
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-11-24 23:48:20 +0300

    Update CORS

--- category_service/settings.py
+++ category_service/settings.py
@@ -169,4 +169,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",  # Add this if you use 'localhost' instead of '127.0.0.1'
     'https://myinsightiq.com',
+    "https://aitoolsmyinisightiq.netlify.app",
 ]
