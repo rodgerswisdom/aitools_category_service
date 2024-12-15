02fa38004678a62fc91d90f4398525e40f594c26
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-12-15 10:38:47 +0300

    Deploy on vercel

--- category_service/settings.py
+++ category_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h(9+v6@(%^#v23gi+tu_!oj45rxm*2r*jgs*m^)0rgz#m#)-)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = []
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
